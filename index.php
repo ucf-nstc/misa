d915471365a493becd5ca0fd924d681c82ac4ea4
tindleaj <tindleaj@gmail.com>
2016-12-13 10:49:35 -0500

    initial commit, added current site

---
+++ index.php
@@ -0,0 +1,90 @@
+<?php $pagetitle = "Home"; require_once('includes/header.php'); ?>
+
+<!-- Info Boxes -->
+<section class="container">
+	<div class="row">
+		<!-- Dates -->
+		<div class="col-sm-4">
+			<h2 class="">
+				<i class="fa fa-calendar-check-o" aria-hidden="true"></i>
+				&nbsp;Dates
+			</h2>
+			<div class="info-box">
+				<h3>Event Dates</h3>
+				<h4>October 3, 2016</h4>
+				<h4>October 4, 2016</h4>
+			</div>
+		</div>
+
+		<!-- Location -->
+		<div class="col-sm-4">
+			<h2 class="">
+				<i class="fa fa-map-marker" aria-hidden="true"></i>
+				&nbsp;Location
+			</h2>
+			<p class="info-box">
+				<img src="images/ucf.jpg"><br>
+				Live Oak Event Center,<br>
+				University of Central Florida,<br>
+				4115 Pyxis Ln, Orlando, FL 32816
+			</p>
+		</div>
+
+		<!-- Contact -->
+		<div class="col-sm-4">
+			<h2 class="">
+				<i class="fa fa-paper-plane" aria-hidden="true"></i>
+				&nbsp;Contact
+			</h2>
+			<p class="info-box">
+				Vilmania Ramos<br>
+				(MISA Program Assistant)<br>
+				<a href="mailto:vramos@ucf.edu">vramos@ucf.edu</a>
+			</p>
+		</div>
+	</div> 
+</section>
+
+<!-- Statement -->
+<section class="container">
+	<div class="statement row">
+		<p class="col-md-6">The materials innovation for sustainable agriculture (MISA) is a center of excellence that has been established to carry out research and education/extension on material innovation to tackle agricultural/citrus diseases. Our program originated from a partnership between US universities, grower cooperatives, agri-chemical industries, regulatory agencies supported through several funded projects from USDA.-NIFA, Citrus Research and Development Foundation, and industry.</p>
+
+		<p class="col-md-6">While it is physically located at the University of Central Florida (UCF), at the heart of Central Florida, we intend for MISA to become a platform for scientists, grower, and industry to meet and exchange the knowledge and skills available to combat agricultural diseases for long-term crop protection. 
+		</p>		
+	</div>
+</section>
+
+<!-- Objectives -->
+<!-- Need to wrap the section in another div to extend background color to 100% width -->
+<div class="objectives">
+	<section class="container">
+		<div class="row">
+			<h2>
+				<i class="fa fa-thumb-tack" aria-hidden="true"></i>
+				&nbsp;MISA 2016 Objectives
+			</h2>
+			<ul>
+				<p>The goal of our MISA center is to: </p>
+				<li>Coordinate research efforts to address agricultural diseases threatening crop yield</li>
+				<li>Improve academic and public education on crop diseases, their economic impact, and possible solutions</li>
+				<li>Develop extension  activities including presence at grower meetings, increasing public awareness, dissemination of research data, tools for economic analysis of treatments by growers, and grower trainings for field deployment of new treatments</li>
+				<li>Foster public-private partnerships to engage all stakeholders in research and extension activities, with the purpose of developing treatment methods that are practical in the field and economically feasible</li>
+				<li>Improve economic returns to rural communities</li>
+			</ul>
+		</div>
+	</section>
+</div>
+
+<section class="container">
+	<div class="row sponsors">
+		<h2>
+			<i class="fa fa-money" aria-hidden="true"></i>
+			&nbsp;Our Sponsors
+		</h2>
+		<a href="http://www.usda.gov/"><img src="images/sponsors/usda.png"></a>
+		<a href="http://ucf.edu"><img src="images/sponsors/ucf.jpg"></a>
+	</div>
+</section>
+
+<?php require_once('includes/footer.php'); ?>
---
+++ onsite.php
@@ -0,0 +1,30 @@
+<?php $pagetitle = "Onsite Info"; require_once('includes/header.php'); ?>
+
+
+<section class="container">
+	<div class="row">
+		<?php require_once('includes/sidebar.php'); ?>
+
+		<div class="onsite col-md-9">
+			<!-- Address -->
+			<h1>Onsite Info</h1>
+			<p>
+				<strong>The Materials Innovation for Sustainable Agriculture Symposium 2016 will take place in the Live Oak Ballroom/Garden Room.</strong><br>
+				Below is information relevant to the event's location, including the address and a link to a live map application.
+			</p>
+
+			<h2>Address</h2>
+			<p>Live Oak Event Center,
+			<br>University of Central Florida, 
+			<br>4115 Pyxis Ln, Orlando, FL 32816
+			</p>
+
+			<a href="https://map.ucf.edu/locations/7ab/live-oak-ballroomgarden-room/">Click here to view the location on a map</a>
+			<a href="https://map.ucf.edu/locations/7ab/live-oak-ballroomgarden-room/"><img src="images/liveoak.png"></a>
+		</div>
+
+	</div>
+</section>
+
+
+<?php require_once('includes/footer.php'); ?>
